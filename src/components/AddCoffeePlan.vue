d7020ce79f2afae1313d4af9dd14fd1dbe71635f
Connie Leung <cleung334@gmail.com>
2025-05-26 01:38:53 +0800

    refactor:  create AddCoffeePlan component

---
+++ src/components/AddCoffeePlan.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+  (e: 'newCoffeePlan', name: string): void
+}>();
+
+const newPlan = ref('');
+function addPlan() {
+    const trimmedPlan = newPlan.value.trim();
+    if (!trimmedPlan) {
+        return;
+    }
+
+    emit('newCoffeePlan', trimmedPlan);
+    newPlan.value = '';
+} 
+</script>
+
+<template>    
+    <form class="add-plan-form" @submit.prevent="addPlan">
+      <input v-model.trim="newPlan" type="text" placeholder="Add a new plan" />
+      <button class="btn btn-primary" type="submit" :disabled="newPlan.length < 5">
+        Add Plan
+      </button>
+    </form>    
+</template>
+
+<style scoped>
+input {
+  padding: 0.5rem 0.75rem;
+}
+
+.add-plan-form {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.add-plan-form input {
+  width: 70%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+</style>
--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import CoffeePlan from './CoffeePlan.vue'
+import AddCoffeePlan from './AddCoffeePlan.vue'
 const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
 
 const selectedPlan = ref('')
@@ -8,25 +9,14 @@ function handleSelectPlan(name: string) {
   selectedPlan.value = name
 }
 
-const newPlan = ref('');
-function addPlan() {
-  if (!newPlan.value) {
-    return;
-  }
-
-  plans.value.push(newPlan.value);
-  newPlan.value = '';
+function addPlan(newPlan: string) {
+  plans.value.push(newPlan);
 } 
 </script>
 
 <template>
   <div class="plans">
-    <form class="add-plan-form" @submit.prevent="addPlan">
-      <input v-model.trim="newPlan" type="text" placeholder="Add a new plan" />
-      <button class="btn btn-primary" type="submit" :disabled="newPlan.length < 5">
-        Add Plan
-      </button>
-    </form>
+    <AddCoffeePlan @newCoffeePlan="addPlan" />
     {{ selectedPlan }}
     <CoffeePlan
       v-for="plan in plans"
@@ -37,28 +27,3 @@ function addPlan() {
     />
   </div>
 </template>
-
-<style scoped>
-input {
-  padding: 0.5rem 0.75rem;
-}
-
-.add-plan-form {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.add-plan-form input {
-  width: 70%;
-  border-radius: 3px;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-  border: 1px solid #f1f5f8;
-  color: #606f7b;
-  padding: 0.5rem 0.75rem;
-  box-sizing: border-box;
-  font-size: 1rem;
-  letter-spacing: 0.5px;
-  margin: 0.5rem 0;
-}
-</style>
